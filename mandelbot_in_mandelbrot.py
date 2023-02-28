2a14f8a2acc344e868327414390dcd5a38bbc8cf
thorn-ale <thornale1@gmail.com>
2023-02-28 10:23:50 +0100

    fun stuff

---
+++ mandelbot_in_mandelbrot.py
@@ -0,0 +1,35 @@
+exec(''.join([''if x in '\n 'else' 'if x=='_'else x for x in"""
+                                                                            
+                                                                            
+                                                    f                       
+                                                   rom                      
+                                                  _PIL                      
+                                                  _imp                      
+                                                ort_Imag e                  
+                                           ;import_sys;sys. se              
+                                           trecursionlimit(900              
+                                         0);(lambda_*a:_Image.              
+                                         frombytes('L',(a[0],a              
+                                        [1]),bytes((lambda_*b:[             
+                             (l am     bda_c:(lambda_d:c(lambda             
+                             _*e:d(d)  (*e)))(lambda_f:c(lambda             
+                            _*g:f(f)(* g))))(lambda_h:(lambda_*             
+                         i  :h(i[0],i[ 1],i[2]*i[2]-i[3]*i[3]+              
+           i[0],2*i[2]*i[3]+i[1],i[4]+1)if_i[2]*i[2]+i[3]*i[                
+                         3  ]<4_and_i[ 4]<b[6]_else_i[4]*a[2]/              
+                            /b[6]))(k* (b[3]-b[2])/b[1]+b[2],j*             
+                             (b[5]-b[  4])/b[0]+b[4],0.0,0.0,0)             
+                             fo r_     j_in_range(b[0])for_k_in             
+                                        _range(b[1])])(a[1],a[0             
+                                         ],a[3],a[4],a[5],a[6]              
+                                         ,a[7]))).save('t.png'              
+                                           ))(800,677,255,-0.7              
+                                           45625,-0.744975, 0.              
+                                                112375,0 .                  
+                                                  1130                      
+                                                  25,1                      
+                                                   000                      
+                                                    )                       
+                                                                            
+"""]))
+
